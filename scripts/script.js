b8449ba876e5873212f6f939b22263b5c8e29e06
Your Name <your_email@example.com>
2023-11-08 15:55:13 +0200

    Add Tasks To List Function

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 let addBtn = document.getElementById("addBtn");
 let taskName = document.getElementById("taskName");
 let priority = document.getElementById("priority");
+let tasksList = document.getElementById("tasksList");
 
 let tasks = [];
 let id = -1;
@@ -25,4 +26,28 @@ addBtn.addEventListener("click", function (e) {
   };
 
   tasks.push(taskObject);
+  addTasksToList(tasks);
 });
+
+function addTasksToList(tasksArray) {
+  tasksList.innerHTML = "";
+  for (let i = 0; i < tasksArray.length; i++) {
+    const taskItem = document.createElement("div");
+    taskItem.innerHTML = `
+        <div class="task" id="task-${i}-name">
+          ${tasksArray[i].taskName}
+        </div>
+        <div class="date" id="task-${i}-date">Date</div>
+        <div class="priority" id="task-${i}-priority">${tasksArray[i].priority}</div>
+        <div class="actions">
+          <button class="btn-icon" name="${i}"><i class="fa-solid fa-check"></i></button>
+          <button class="btn-icon" name="${i}">
+            <i class="fa-regular fa-pen-to-square"></i>
+          </button>
+          <button class="btn-icon" name="${i}"><i class="fa-solid fa-trash"></i></button>
+        </div>
+        `;
+    taskItem.setAttribute('class','content-row')
+    tasksList.appendChild(taskItem);
+  }
+}
